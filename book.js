b5efb3ef0606cf5e421021a2cce4ebbcc9d4a619
无星 <32910694@qq.com>
2021-04-07 22:10:59 +0800

    init

---
+++ book.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Xing.Wu
+ * @Date: 2021-04-07 22:06:25
+ * @LastEditTime: 2021-04-07 22:06:54
+ * @LastEditors: Xing.Wu
+ * @Description: In User Settings Edit
+ * @FilePath: /笔记/book.js
+ */
+  
+module.exports = {
+    // Enforce use of GitBook v3
+    gitbook: '3.2.3',
+    language: "zh-hans",
+    plugins:[
+    ],
+    pluginsConfig:{
+    }
+};
